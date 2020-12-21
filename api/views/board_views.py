d8aff0165b370d13d2c518ccfb26331b12daee3a
JeheeChoi <writetojehee@gmail.com>
2020-12-20 19:24:39 -0800

    Built Board detail
    added Show/delete on board views

--- api/views/board_views.py
+++ api/views/board_views.py
@@ -10,6 +10,8 @@ from django.middleware.csrf import get_token
 from ..models.board import Board
 from ..serializers import BoardSerializer, UserSerializer
 
+# Index Boards - Signed in users can view all the boards created
+# Create boards - with ownership
 class Boards(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = BoardSerializer
@@ -33,3 +35,23 @@ class Boards(generics.ListCreateAPIView):
           board.save()
           return Response({ 'board': board.data }, status=status.HTTP_201_CREATED)
       return Response(board.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Show single board - no ownership required. any signed in user can view the board detail
+# Delete board - ownership required.
+class BoardDetail(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (IsAuthenticated,)
+    def get(self, request, pk):
+      """Show request"""
+      board = get_object_or_404(Board, pk=pk)
+      # if not request.user.id == board.owner.id:
+      #     raise PermissionDenied('Unauthorized, you do not own this board')
+      data = BoardSerializer(board).data
+      return Response({ 'board': data })
+
+    def delete(self, request, pk):
+      """Delete request"""
+      board = get_object_or_404(Board, pk=pk)
+      if not request.user.id == board.owner.id:
+          raise PermissionDenied('Unauthorized, you do not own this board')
+      board.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
